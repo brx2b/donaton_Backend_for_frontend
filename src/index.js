fa4087d0e8f8fb19e378e7d5a4ff88a38f28e79d
Brx2b <briansnowshine@gmail.com>
2026-05-10 22:37:08 -0400

    puerto corst ip front

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app=express();
 const PORT=process.env.PORT || 4000;
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'], // ambos puertos por si acaso
+    origin: ['http://localhost:5173', 'http://localhost:5174','http://52.87.217.73:5173'], // ambos puertos por si acaso
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
