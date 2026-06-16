2ba0d4b6e5915b627d8c3e9c4467ee82c18ba6ee
Brx2b <briansnowshine@gmail.com>
2026-06-16 02:18:35 -0400

    docker local

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
-const GATEWAY_URL = process.env.GATEWAY_URL || "http://localhost:8086";
+const GATEWAY_URL = process.env.GATEWAY_URL || "http://host.docker.internal:8086";
 
 app.use(helmet({
   contentSecurityPolicy: false, // Disable CSP for dev
