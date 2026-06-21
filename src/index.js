db783554c350b19c2cecf8ae8c3d85f1c1e5e66f
Brx2b <briansnowshine@gmail.com>
2026-06-21 19:43:51 -0400

    ip nuevva

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 app.use(cors({
-    origin: ["http://13.222.223.222","10.0.158.181",'http://localhost:5173', 'http://localhost:5174','http://52.87.217.73:5173','http://52.87.217.73','http://52.87.217.73:4000'],
+    origin: ["52.91.255.222","http://13.222.223.222","10.0.158.181",'http://localhost:5173', 'http://localhost:5174','http://52.87.217.73:5173','http://52.87.217.73','http://52.87.217.73:4000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
